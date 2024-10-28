29d584a1cccd4525318005b75971a5871041132b
Himanshu Singh <hsgbxrssm@gmail.com>
2024-10-29 00:00:36 +0530

    update

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ app.use(
 // middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+//app.use(cors());
 // to serve images inside public folder
 app.use(express.static('public')); 
 app.use('/images', express.static('images'));
